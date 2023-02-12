b374517f4fec840eb3c2dae73de7a2c03e581552
Nancy Hunter <hunternancym@gmail.com>
2023-02-12 19:10:48 +0700

    21st commit

--- regex test for PIN code.js
+++ regex test for PIN code.js
@@ -12,3 +12,10 @@
 function validatePIN (pin) {
     return pin.length == 4 && /\d{4}/.test(pin) || pin.length==6 && /\d{6}/.test(pin)
   }
+
+  //sleeker
+
+
+  function validatePIN (pin) {
+    return  /^\d{4}$/|/^\d{6}$/.test(pin) 
+  }
