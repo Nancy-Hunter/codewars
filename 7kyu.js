e4b4311cb8848a17b4d986f389a0e8bdd07cb715
Nancy Hunter <hunternancym@gmail.com>
2023-11-04 19:49:16 -0400

    daily

--- 7kyu.js
+++ 7kyu.js
@@ -11,3 +11,24 @@ function squareDigits(num){
               .join("")
             
 } 
+
+// DESCRIPTION:
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+	return s.toUpperCase()
+          .split("")
+          .map((x, index)=> {
+              let answer = x
+              for(let i =0; i<index; i++){
+                answer += x.toLowerCase()
+              }
+              return answer
+          })
+          .join("-")
+}
