90dfc48d276d044cadeba263261a004a14ec9c47
Nancy Hunter <hunternancym@gmail.com>
2023-12-26 19:32:34 +0500

    two to one

---
+++ training on two to one.js
@@ -0,0 +1,25 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+
+function longest(s1, s2) {
+    let result =[]
+    for (let i = 0; i<s1.length; i++) {
+      if (!result.includes(s1[i])){
+          result.push(s1[i])
+          }
+    }
+    for (let i = 0; i<s2.length; i++) {
+      if (!result.includes(s2[i])){
+          result.push(s2[i])
+          }
+    }
+    return result.sort().join('')
+  }
