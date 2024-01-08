020125f29703a40c5e1daf66ee179cc6e0dcbf90
Nancy Hunter <hunternancym@gmail.com>
2024-01-08 10:40:11 +0700

    char code calculation

---
+++ Char code calculation.js
@@ -0,0 +1,25 @@
+// Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+// 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+// Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+// total1 = 656667
+//               ^
+// total2 = 656661
+//               ^
+// Then return the difference between the sum of the digits in total1 and total2:
+
+//   (6 + 5 + 6 + 6 + 6 + 7)
+// - (6 + 5 + 6 + 6 + 6 + 1)
+// -------------------------
+//                        6
+
+
+function calc(x){
+    let xArray = x.split('').map(x=>x.charCodeAt()).join('')
+    
+    let total2 = xArray.split('').map(x=> x == 7? 1 : x).reduce((acc, current)=> acc + current)
+    let total1 = xArray.split('').reduce((acc, current)=> acc + current, 0)
+    console.log(total2)
+    return Number(total1-total2)
+  }
