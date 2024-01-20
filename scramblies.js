0a430340b58e8ca9ac41f46364294887f46b36be
Nancy Hunter <hunternancym@gmail.com>
2024-01-20 19:45:30 +0700

    refactored scramblies and added orate clock

--- scramblies.js
+++ scramblies.js
@@ -22,3 +22,10 @@ function scramble(str1, str2) {
   }
 
   //times out
+
+  function scramble(str1, str2) {
+    let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
+    return str2.split("").every((character) => --occurences[character] >= 0);
+  }
+
+  //best solution
