ea644e98ce54e7249dde7a51c7c7b00f7df2644a
Nancy Hunter <hunternancym@gmail.com>
2024-02-01 10:18:09 +0700

    switch statement classic

---
+++ Round to the next highest 5.js
@@ -0,0 +1,34 @@
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+
+function roundToNext5(n){
+    if (n % 5 == 0) {
+      return n
+    }
+    if (n < 0) {
+      return (Math.ceil(Math.abs(n)/5)-1) * -5
+    }
+    return (Math.floor(n/5)+1) * 5  
+  }
+
+
+  //so much better
+
+  function roundToNext5(n){
+    return Math.ceil(n/5)*5;
+  }
