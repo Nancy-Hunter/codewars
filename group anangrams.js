ee875bc7edf303a207a38521ac6a22b5a732c87d
Nancy Hunter <hunternancym@gmail.com>
2024-03-06 21:50:18 +0700

    group anagrams finished

--- group anangrams.js
+++ group anangrams.js
@@ -20,7 +20,7 @@
 // I'd advise you to find an efficient way for grouping the words in anagrams otherwise you'll probably won't pass the heavy superhero test cases
 
 
-//works but is too slow for giant tests have to take out console.logs to work for superhero tests
+//works but is too slow for giant tests have to take out console.logs to work for superhero tests. 
 
 function groupAnagrams(words){
   let sortedWords = words.map((current, index, arr)=> current.split('').sort().join(''))
