9e9abca74a60a3ff57c7a7e0579c07734277c383
Nancy Hunter <hunternancym@gmail.com>
2024-04-26 13:19:24 +0700

    frogs dinner

--- multiples of 3  or 5.js
+++ multiples of 3  or 5.js
@@ -9,9 +9,7 @@
 function solution(number){
     let result = 0
     for (let i = 3; i < number; i++) {
-      if (i % 3 == 0 && i % 5 == 0) {
-        result += i
-      } else if (i % 3==0 || i % 5==0) {
+      if (i % 3==0 || i % 5==0) {
         result += i
       }
     }
