a79dcfaa93449186980149739c88e6ffee9f6c77
Nancy Hunter <hunternancym@gmail.com>
2024-05-26 23:20:40 +0700

    reverse list order

---
+++ sort arrays ignoring case.js
@@ -0,0 +1,18 @@
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+sortme = function( names ){
+    return names.sort((a, b) => {
+      const nameA = a.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+  })
+  }
