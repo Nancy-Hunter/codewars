9dd48de4483e5bf4d1417b5087f181b3e38f6b45
Nancy Hunter <hunternancym@gmail.com>
2024-06-14 09:06:43 +0700

    data reverse

---
+++ data reverse.js
@@ -0,0 +1,27 @@
+// A stream of data is received and needs to be reversed.
+
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+// 11111111  00000000  00001111  10101010
+//  (byte1)   (byte2)   (byte3)   (byte4)
+// should become:
+
+// 10101010  00001111  00000000  11111111
+//  (byte4)   (byte3)   (byte2)   (byte1)
+// The total number of bits will always be a multiple of 8.
+
+// The data is given in an array as such:
+
+// [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+function dataReverse(data) {
+    let result = [] 
+    let temp 
+    let counter = 0
+    for (let i = 0; i < data.length/8; i++) {
+      temp = data.slice(counter, counter + 8)
+      result.unshift(temp)
+      counter += 8 
+    }
+    return result.flat()
+  }
