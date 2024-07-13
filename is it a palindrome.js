f83ac7a3992760f43e1ae12a8d2061a379e56da2
Nancy Hunter <hunternancym@gmail.com>
2024-07-13 17:44:52 -0400

    palindrome

--- is it a palindrome.js
+++ is it a palindrome.js
@@ -3,6 +3,5 @@
 // A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
 
 function isPalindrome(x) {
-    let half = Math.round(x.length/2)
-    return x.toLowerCase().slice(0, half) == x.toLowerCase().slice(-half).split('').reverse().join('')
+    return x.toLowerCase() == x.toLowerCase().split('').reverse().join('')
   }
