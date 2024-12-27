88da365442ecfb50d7d4db21a2447abb5da5632c
Nancy Hunter <hunternancym@gmail.com>
2024-12-27 14:09:58 -0500

    array plus array python

--- array plus array.js
+++ array plus array.js
@@ -5,3 +5,13 @@
 function arrayPlusArray(arr1, arr2) {
     return [...arr1, ...arr2].reduce((acc, el)=> acc + el, 0)
   }
+
+
+  // IN PYTHON
+
+  // def array_plus_array(arr1,arr2):
+  //   arr = arr1 + arr2
+  //   sum = 0
+  //   for i in range(len(arr)):
+  //       sum += arr[i]
+  //   return sum
