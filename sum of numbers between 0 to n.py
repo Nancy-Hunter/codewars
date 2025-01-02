adc0aa49b8807a787fda362d9091973f7cf73441
Nancy Hunter <hunternancym@gmail.com>
2025-01-02 10:21:37 -0500

    python using join and map

--- sum of numbers between 0 to n.py
+++ sum of numbers between 0 to n.py
@@ -34,3 +34,15 @@ def show_sequence(n):
         if (i == n) : result += f'{str(i)}'
         else : result+= f'{str(i)}+'
     return f'{result} = {sum(arr)}'
+
+
+# //BETTER using join and map
+
+def show_sequence(n):
+    if n == 0:
+        return "0=0"
+    elif n < 0:
+        return str(n) + "<0"
+    else:
+        counter = sum(range(n+1))
+        return '+'.join(map(str, range(n+1))) + " = " + str(counter)
