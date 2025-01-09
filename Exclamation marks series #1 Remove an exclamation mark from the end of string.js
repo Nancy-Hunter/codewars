c01e256bbd6bb64cd2988ca7b30c46d72ec562d4
Nancy Hunter <hunternancym@gmail.com>
2025-01-09 16:18:34 -0500

    python update

--- Exclamation marks series #1 Remove an exclamation mark from the end of string.js
+++ Exclamation marks series #1 Remove an exclamation mark from the end of string.js
@@ -12,3 +12,18 @@
 function remove (string) {
     return string[string.length-1] == '!' ? string.slice(0, string.length-1) : string
   }
+
+  // in python 
+
+  // def replace_exclamation(st):
+  //   vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  //   for el in vowels :
+  //       st = st.replace(el, "!")
+  //   return  st 
+
+  // def replace_exclamation(s):
+	// for i in "aeuioAEUIO":	s = s.replace(i, "!")
+	// return s
+
+  // def replace_exclamation(s):
+  //   return ''.join('!' if c in 'aeiouAEIOU' else c for c in s)
