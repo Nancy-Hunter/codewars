a024d434a44d603f69430313c3d6031ffc6be7e4
Nancy Hunter <hunternancym@gmail.com>
2025-01-24 18:30:38 -0500

    youre a square python

--- You're a square.js
+++ You're a square.js
@@ -37,4 +37,12 @@ var isSquare = function(n){
       return Math.sqrt(n) % 1 == 0
   }
 
+// in python 
+
+//   import math
+
+// def is_square(n):    
+//     if n < 0 :
+//         return False
+//     return math.sqrt(n) % 1 == 0
 
