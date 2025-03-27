adaa28122c981c5c15d29383fdc099f1147563eb
Nancy Hunter <hunternancym@gmail.com>
2025-03-27 11:18:52 -0400

    sum of numbers between 0 to n

---
+++ sum of numbers between 0 to n.py
@@ -0,0 +1,36 @@
+# Description:
+
+# We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+# Example:
+# Input:
+
+# > 6
+# Output:
+
+# 0+1+2+3+4+5+6 = 21
+
+# Input:
+
+# > -15
+# Output:
+
+# -15<0
+
+# Input:
+
+# > 0
+# Output:
+
+# 0=0
+
+def show_sequence(n):
+    if (not n) : return '0=0'
+    if (n < 0) : return f'{n}<0'
+    arr = []
+    result =''
+    for i in range(n+1):
+        arr.append(i)
+        if (i == n) : result += f'{str(i)}'
+        else : result+= f'{str(i)}+'
+    return f'{result} = {sum(arr)}'
