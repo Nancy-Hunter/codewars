6017f7d60d5e03f513c56107ab6dfff2b12c18f3
Nancy Hunter <hunternancym@gmail.com>
2025-06-09 18:29:24 -0400

    simple consecutive pairs without making array length even

--- simple consecutive pairs.js
+++ simple consecutive pairs.js
@@ -13,9 +13,6 @@
 
 function pairs(ar){
   let count = 0
-  if (ar.length % 2 != 0) {
-    ar.pop()
-  }
   for (let i = 0; i < ar.length; i+=2) {
     if (Math.abs(ar[i]-ar[i+1])==1) {
       count++
