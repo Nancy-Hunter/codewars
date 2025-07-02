0d8dc0b135886506b583bfc8f41979e1d479a2fa
Nancy Hunter <hunternancym@gmail.com>
2025-07-01 22:55:57 -0400

    merge 2 arrays into one unique array another option with array.from

--- merge two arrays into one unique array.js
+++ merge two arrays into one unique array.js
@@ -18,3 +18,8 @@
 function mergeArrays(arr1, arr2) {
   return [...new Set([...arr1, ...arr2])].sort((a, b)=> a - b)
 }
+
+//another option
+function mergeArrays(arr1, arr2) {
+  return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+}
