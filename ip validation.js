1bde6b2673f93dbee7cc18c9dfbbaecbeb0f51ad
Nancy Hunter <hunternancym@gmail.com>
2025-07-22 21:06:43 -0400

    ip validation better

--- ip validation.js
+++ ip validation.js
@@ -23,3 +23,16 @@ function isValidIP(str) {
   if (!tetra.join('').split('').every(el=> numbers.includes(el))) return false    
   return true
 }
+
+// BETTER
+
+function isValidIP(str) {
+  const octets = str.split('.')							// Split into octets
+  return (octets.length === 4) &&						// Ensure str has exactly 4 octets
+  	octets.reduce((acc, octet) =>						// Check all octets
+    	(acc === true) &&												// Ensure prior octets okay
+     	(String(Number(octet)) === octet) &&		// Ensure octet has nothing funky like spaces, leading 0's, ...
+  		(Number(octet) >= 0) &&									// Ensure octet is 0 or more
+   		(Number(octet) <= 255)									// Ensure octet is 255 or less
+  	, true)
+}
